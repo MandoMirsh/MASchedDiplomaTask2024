2033f904211b18265453708e50d0739d4c3c2c87
MandoMirsh <33557863+MandoMirsh@users.noreply.github.com>
2024-01-02 02:04:21 +0300

    Problem model added

---
+++ src/Main/models/ProblemModel.java
@@ -0,0 +1,26 @@
+package Main.models;
+
+import java.util.ArrayList;
+
+public class ProblemModel {
+    private ArrayList<Integer> rNum = new ArrayList<>();
+    private ArrayList<TaskModel> tasks = new ArrayList<>();
+    public void setRes(int num, int volume) {
+        rNum.add(num - 1, volume);
+    }
+    public void setRes(int resVol) {
+        rNum.add(resVol);
+    }
+    public int getRnum() {
+        return rNum.size();
+    }
+    public int getRes(int resPos) {
+        return rNum.get(resPos - 1);
+    }
+    public void addTask(TaskModel task, int taskId) {
+        tasks.add(taskId, task);
+    }
+    public ArrayList<TaskModel> getTasks() {
+        return new ArrayList<TaskModel>(tasks);
+    }
+}
---
+++ src/Main/models/TaskModel.java
@@ -0,0 +1,42 @@
+package Main.models;
+
+import java.util.ArrayList;
+
+public class TaskModel {
+
+    private int selfNumber, timeNeed;
+    private ArrayList<Integer> resourceNeed = new ArrayList<>();
+    private ArrayList<Integer> sucsessors = new ArrayList<>();
+
+    public void setResourceNeed ( int resVolume, int resPos) {
+        resourceNeed.set(resPos-1, resVolume);
+    }
+    public void setResourceNeed ( int resVolume) {
+        resourceNeed.add(resVolume);
+    }
+    public int getResourceNeed (int resPos) {
+        return resourceNeed.get(resPos-1);
+    }
+    public void setTimeNeed( int time) {
+        timeNeed = time;
+    }
+    public int getTimeNeed(){
+        return timeNeed;
+    }
+
+    public TaskModel(int selfPos) {
+        this.selfNumber = selfPos;
+    }
+    public TaskModel(){
+        this.selfNumber = 0;
+    };
+    public void setSelfPos(int pos) {
+        this.selfNumber = pos;
+    }
+    public void setSucsessor ( int sucPos) {
+        sucsessors.add(sucPos);
+    }
+    public ArrayList<Integer> getSuccessors() {
+        return sucsessors;
+    }
+}
