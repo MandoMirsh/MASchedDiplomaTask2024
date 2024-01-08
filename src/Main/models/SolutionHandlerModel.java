0f2606b1922b7debfbc3141b59f09254508de88b
MandoMirsh <33557863+MandoMirsh@users.noreply.github.com>
2024-01-09 01:29:16 +0300

    Added solutionHandler - a model which processes project tasks all around  file checkers, file readers, solvers and marker.

---
+++ src/Main/models/SolutionHandlerModel.java
@@ -0,0 +1,134 @@
+package Main.models;
+import Main.DTO.InputFileDTO;
+import Main.DTO.SolutionControllerTaskDTO;
+import Main.Marker;
+import Main.loggers.LogHandler;
+import Main.readers.FileReader;
+import Main.readers.RCPFileReader;
+import Main.solvers.MockSolver;
+import Main.solvers.Solver;
+import Main.checkers.*;
+public class SolutionHandlerModel {
+    private Solver currentSolver;
+    private ProblemModel currentProblem;
+    private FileChecker currentChecker;
+    private FileReader currentReader;
+    private Marker marker = new Marker();
+    private SolutionControllerTaskDTO commonTask;
+    public static final int SOL_SET_TASK_PLEASE = 0, SOL_READY = 1, SOL_IN_PROGRESS = 2, SOL_CHECKS = 3;
+    private LogHandler log, results;
+    private String solverName;
+    public void setProblem(SolutionControllerTaskDTO task) {
+        commonTask = task;
+    }
+    public void startSolving() {
+
+        currentChecker.setFile(fullFileName(commonTask.getFileInfo()));
+        if(!currentChecker.check()){
+            log.write("Wrong file format!");
+            log.write(fullFileName(commonTask.getFileInfo()));
+        }
+        else{
+            currentProblem = currentReader.readFile(fullFileName(commonTask.getFileInfo()));
+            currentSolver.setTask(currentProblem);
+            currentSolver.runSolver();
+            while(currentSolver.getSolutionStatus() == 0);
+            if (currentSolver.getSolutionStatus() != 1) {
+                log.write("Unknown error while solving!");
+            }
+            else {
+                int result = currentSolver.getLongevity();
+                Marker.setResult(result);
+                int set = commonTask.getFileInfo().getProblemClass();
+                int resultMark = Marker.getSolutionMark(set,commonTask.getFileInfo().getDecNum(),
+                                                            commonTask.getFileInfo().getPosition());
+                String resultToLog = "j" + set + "0" + commonTask.getFileInfo().getDecNum() + "_"
+                                        + commonTask.getFileInfo().getPosition()+ "," + solverName + ","
+                                        + result +"," + (Marker.getAnswer(commonTask.getFileInfo().getDecNum(),
+                                            commonTask.getFileInfo().getPosition())) + "," + resultMark;
+                results.write(resultToLog);
+            }
+
+        }
+
+    }
+    public void makeReady() {
+        setSolver(commonTask.getSolverType());
+        setCheckerReader(commonTask.getFileInfo().getExtension());
+        currentSolver.setLogger(log);
+    }
+    public void runNext(){
+        nextTask();
+        currentChecker.setFile(fullFileName(commonTask.getFileInfo()));
+        if(!currentChecker.check()){
+            log.write("Wrong file format!");
+            log.write(fullFileName(commonTask.getFileInfo()));
+        }
+        else{
+            currentProblem = currentReader.readFile(fullFileName(commonTask.getFileInfo()));
+            currentSolver.setTask(currentProblem);
+            currentSolver.runSolver();
+            while(currentSolver.getSolutionStatus() == 0);
+            if (currentSolver.getSolutionStatus() != 1) {
+                log.write("Unknown error while solving!");
+            }
+            else {
+                int result = currentSolver.getLongevity();
+                Marker.setResult(result);
+                int set = commonTask.getFileInfo().getProblemClass();
+                int resultMark = Marker.getSolutionMark(set,commonTask.getFileInfo().getDecNum(),
+                        commonTask.getFileInfo().getPosition());
+                String resultToLog = "j" + set + "0" + commonTask.getFileInfo().getDecNum() + "_"
+                        + commonTask.getFileInfo().getPosition()+ "," + solverName + ","
+                        + result +"," + (Marker.getAnswer(commonTask.getFileInfo().getDecNum(),
+                        commonTask.getFileInfo().getPosition())) + "," + resultMark;
+                results.write(resultToLog);
+            }
+
+        }
+    }
+    private void nextTask(){
+        if (commonTask.getFileInfo().getPosition() == 10) {
+            commonTask.getFileInfo().setPosition(1);
+            int decnum = commonTask.getFileInfo().getDecNum() + 1;
+            commonTask.getFileInfo().setDecNum(decnum);
+        }
+        else
+        {
+            int pos = commonTask.getFileInfo().getPosition() + 1;
+            commonTask.getFileInfo().setPosition(pos);
+        }
+    }
+
+
+    public void setLog (LogHandler logPlace, LogHandler resultPlace) {
+            log = logPlace;
+            results = resultPlace;
+    }
+    private void setSolver(int solverId){
+        switch(solverId) {
+            case SolutionControllerTaskDTO.GREEDY_SOLVER -> {currentSolver = new MockSolver(); solverName = "Greedy";}//GreedySolver();
+            case SolutionControllerTaskDTO.CPLEX_SOLVER -> {currentSolver = new MockSolver(); solverName = "CPLEX";}//CPLEXSolver();
+            case SolutionControllerTaskDTO.MA_SOLVER -> {currentSolver = new MockSolver(); solverName = "Multi-Agent";}//MASolver();
+            default -> currentSolver = new MockSolver();
+        }
+    }
+    private void setCheckerReader(String extention){
+        if (extention.toUpperCase().equals("RCP")) {
+            currentChecker = new RCPFileChecker();
+            currentReader = new RCPFileReader();
+        }
+        else {
+           // currentChecker = new SMChechecker();
+           // currentReader = new SMFileReader();
+        }
+
+    }
+    private String fullFileName (InputFileDTO fileInfo) {
+        String filename = fileInfo.getPath() + "j" + fileInfo.getProblemClass() + "0" + fileInfo.getDecNum() + "_" +
+                fileInfo.getPosition() + "." + fileInfo.getExtension();
+        return filename;
+    }
+
+}
+
